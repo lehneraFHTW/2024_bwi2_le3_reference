5fdd43457874c0c6645c1e04c014eb841b7d2712
lehnera <lehnera@technikum-wien.at>
2024-06-16 12:28:59 +0200

    add substraction feature

--- src/main/java/TestApplication.java
+++ src/main/java/TestApplication.java
@@ -2,5 +2,6 @@ public class TestApplication {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println(calc.add(1,2));
+        System.out.println(calc.substract(5,3));
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -28,4 +28,31 @@ class CalculatorTest {
         //Assert
         Assertions.assertEquals(-3,result);
     }
+
+    @Test
+    void substract_twoPositiveIntegersBiggerFirst_ReturnsPositiveResult(){
+        //Arrange
+        Calculator calc = new Calculator();
+        int result;
+
+        //Act
+        result = calc.substract(5,2);
+
+        //Assert
+        Assertions.assertEquals(3,result);
+
+    }
+
+    @Test
+    void substract_twoPositiveIntegersSmallerFirst_ReturnsNegativeResult(){
+        //Arrange
+        Calculator calc = new Calculator();
+        int result;
+        //Act
+        result = calc.substract(2,5);
+
+        //Assert
+        Assertions.assertEquals(-3,result);
+
+    }
 }
